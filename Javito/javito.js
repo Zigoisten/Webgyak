1f8ac368e161ad2fa9e38d56c21d546336360fc2
Táborosi Máté Kristóf <78024133+Zigoisten@users.noreply.github.com>
2023-10-17 09:40:50 +0200

    Add files via upload

---
+++ Javito/javito.js
@@ -0,0 +1,6 @@
+document.body.style.backgroundColor = "#CCFFFF";
+
+document.getElementsByClassName("container").style.backgroundColor = "#12406A";
+document.getElementById("elso").style.backgroundColor = "#EB8F90";
+document.getElementById("masodik").style.backgroundColor = "#FFB471";
+document.getElementById("harmadik").style.backgroundColor = "#ADBED2";
